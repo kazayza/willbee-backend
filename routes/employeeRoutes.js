5538b5368c6014703afb631a16d95c5d15316546
Ahmed Mohamed <kazayza@yahoo.com>
2026-01-18 23:00:16 +0200

    Correct sort in employee routes

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -5,17 +5,28 @@ const empController = require('../controllers/employeeController');
 // عرض الموظفين (يدعم ?activeOnly=false و ?search=...)
 router.get('/', empController.getEmployees);
 
+// الموظفين اللى ليهم يوزرز بس
+// GET /api/employees/with-users
+router.get('/with-users', empController.getEmployeesWithUsers);
+
+// جلب قائمة الوظائف
+// GET /api/employees/jobs
+router.get('/jobs', empController.getEmployeeJobs);
+
 // إضافة موظف
+// POST /api/employees
 router.post('/', empController.createEmployee);
 
 // سجل الرواتب لموظف معين
+// GET /api/employees/:id/salary
 router.get('/:id/salary', empController.getEmployeeSalaryHistory);
 
-// جلب قائمة الوظائف
-router.get('/jobs', empController.getEmployeeJobs);
+// جلب موظف واحد بالـ ID
+// GET /api/employees/:id
 router.get('/:id', empController.getEmployeeById);
+
+// تعديل موظف
+// PUT /api/employees/:id
 router.put('/:id', empController.updateEmployee);
-// الموظفين اللى ليهم يوزرز بس
-router.get('/with-users', empController.getEmployeesWithUsers);
 
 module.exports = router;
